b2f97c3dd76ed6e6fba0914022a37ce52bff397e
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-13 13:19:02 +0100

    Create controlled elements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
+
+
+
 export default function Main() {
+
+    const [formData, setFormData] = useState({
+        "title": '',
+        "author": '',
+        "body": '',
+        "public": false
+    })
+
+    function handleSubmit(e) {
+
+    }
+
+
+
 
     return (
 
@@ -8,25 +26,39 @@ export default function Main() {
                     <div class="mb-3">
                         <label for="author" class="form-label">Author</label>
                         <input
+                            name="author"
+                            value={formData.author}
+                            onChange={e => setFormData({ ...formData, author: e.target.value })}
                             type="text" class="form-control"
                             id="author" placeholder="Author name" />
                     </div>
                     <div class="mb-3">
                         <label for="title" class="form-label">Author</label>
                         <input
+                            name="title"
+                            value={formData.title}
+                            onChange={e => setFormData({ ...formData, title: e.target.value })}
                             type="text" class="form-control"
                             id="title" placeholder="Title" />
                     </div>
                     <div class="mb-3">
                         <label for="body" class="form-label">Article text</label>
-                        <textarea class="form-control" id="body" rows="3"></textarea>
+                        <textarea
+                            name="body"
+                            value={formData.body}
+                            onChange={e => setFormData({ ...formData, body: e.target.value })}
+                            class="form-control" id="body"
+                            rows="3"></textarea>
                     </div>
                     <div class="form-check">
                         <input
+                            name="public"
+                            checked={formData.public}
+                            onChange={e => setFormData({ ...formData, public: e.target.checked })}
                             class="form-check-input" type="checkbox"
-                            value="" id="checkDefault" />
-                        <label class="form-check-label" for="checkDefault">
-                            Default checkbox
+                            id="public" />
+                        <label class="form-check-label" htmlFor="public">
+                            Public
                         </label>
                     </div>
                 </form>
