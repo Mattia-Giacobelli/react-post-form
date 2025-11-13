dc63a194cfa858a587ef3b8164e0cb88005ed2f5
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-13 12:46:40 +0100

    Create form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ export default function Header() {
     return (
 
         <header>
-
+            <h1 className="text-center p-4 bg-success-subtle">
+                FORM POST
+            </h1>
         </header>
 
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,34 @@ export default function Main() {
     return (
 
         <main>
+            <div className="container">
+                <form >
+                    <div class="mb-3">
+                        <label for="author" class="form-label">Author</label>
+                        <input
+                            type="text" class="form-control"
+                            id="author" placeholder="Author name" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Author</label>
+                        <input
+                            type="text" class="form-control"
+                            id="title" placeholder="Title" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="body" class="form-label">Article text</label>
+                        <textarea class="form-control" id="body" rows="3"></textarea>
+                    </div>
+                    <div class="form-check">
+                        <input
+                            class="form-check-input" type="checkbox"
+                            value="" id="checkDefault" />
+                        <label class="form-check-label" for="checkDefault">
+                            Default checkbox
+                        </label>
+                    </div>
+                </form>
+            </div>
 
         </main>
 
